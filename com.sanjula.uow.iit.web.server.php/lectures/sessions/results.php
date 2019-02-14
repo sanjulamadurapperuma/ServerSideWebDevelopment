fc7e818d5db982c05f76ea359b4e58dba0ddf9a0
Sanjula Madurapperuma <sanjula99@gmail.com>
2019-02-14 20:24:06 +0530

    Corrected incorrect output patterns in Sessions

--- com.sanjula.uow.iit.web.server.php/lectures/sessions/results.php
+++ com.sanjula.uow.iit.web.server.php/lectures/sessions/results.php
@@ -4,7 +4,7 @@ $age = $_GET['age'];
 if($age < 18){
 	$diff = 18 - $age;
 	echo "Underage. Try in ".$diff." years";
-} else if($age >= 18){
+} else if($age >= 18){ 
 	echo "Eligible to drive";
 }
 ?>
