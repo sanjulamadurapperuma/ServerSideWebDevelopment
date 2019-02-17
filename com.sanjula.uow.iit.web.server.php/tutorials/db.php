6fd929945cbc94057d784b85db8a2dd852dc2477
Sanjula Madurapperuma <sanjula99@gmail.com>
2019-02-17 21:47:16 +0530

    Added template.php

--- com.sanjula.uow.iit.web.server.php/tutorials/db.php
+++ com.sanjula.uow.iit.web.server.php/tutorials/db.php
@@ -2,7 +2,7 @@
 $dbhost = 'localhost';
 $dbuser = 'root';
 $dbpass = '';
-$dbname = 'homteq';
+$dbname = 'HOMTEQ';
 //create a DB connection
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) ;
 //if the DB connection fails, display an error message and exit
