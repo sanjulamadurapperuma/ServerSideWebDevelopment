cfa7531cd0fcd321466c2bf0556dc0e88bb5d86d
Sanjula Madurapperuma <sanjula99@gmail.com>
2019-02-21 07:48:11 +0530

    Fixed indentation issues //TODO - Organize for Week 1 and 2

--- com.sanjula.uow.iit.web.server.php/tutorials/tutorial4/prodbuy.php
+++ com.sanjula.uow.iit.web.server.php/tutorials/tutorial4/prodbuy.php
@@ -5,25 +5,30 @@
     echo "<body>";
     include("headfile.html"); //include header layout file
     echo "<h4>".$pagename."</h4>"; //display name of the page on the web page
+
     //retrieve the product id passed from previous page using the GET method and the $_GET superglobal variable
     //applied to the query string u_prod_id
     //store the value in a local variable called $prodid
     $prodid=$_GET['u_prod_id'];
+
     //display the value of the product id, for debugging purposes
     echo "<p>Selected product Id: ".$prodid;
+
     //create a $SQL variable and populate it with a SQL statement that retrieves product details
     $SQL="select prodId, prodName, prodPicNameLarge, prodDescripLong, prodPrice, prodQuantity from Product WHERE prodId =".$prodid; //run SQL query for connected DB or exit and display error message
     $exeSQL=mysqli_query($conn, $SQL) or die (mysqli_error());
     echo "<table style='border: 0px'>";
+
     //create an array of records (2 dimensional variable) called $arrayp.
     //populate it with the records retrieved by the SQL query previously executed.
     //Iterate through the array i.e while the end of the array has not been reached, run through it
     while ($arrayp=mysqli_fetch_array($exeSQL)) { echo "<tr>"; echo "<td style='border: 0px'>";
-    //display the small image whose name is contained in the array
 
+    //display the small image whose name is contained in the array
     //make the image into an anchor to prodbuy.php and pass the product id by URL (the id from the array)
     echo "<a href=prodbuy.php?u_prod_id=".$arrayp['prodId'].">";
     echo "<img src=images/".$arrayp['prodPicNameLarge']." height=200 width=200>";
+
     //close the anchor
     echo "</a>";
     echo "</td>";
@@ -32,6 +37,7 @@
     echo "<p><p>".$arrayp['prodDescripLong']."</p>";
     echo "<p><p><b>&pound".$arrayp['prodPrice']."</b></p>";
     echo "<p><p>Number left in stock: ".$arrayp['prodQuantity']."</p>";
+
     //display product name as contained in the array
     echo "</td>"; echo "</tr>";
     echo "</table>";
